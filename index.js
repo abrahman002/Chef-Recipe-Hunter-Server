12c70320ab9168dea4e67af0ddeecd0d0f4e09b5
abrahman002 <rahmanx661@gmail.com>
2023-05-02 12:20:41 +0600

    added json file

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const express =require('express')
 const app=express()
 const port=4000;
 // const cors=require(cors)
+const recipe=require('./data/chef.json')
 
 // app.use(cors());
 
+app.get('/recipe',(req,res)=>{
+    res.send(recipe);
+})
+
+app.get('/recipe/:id',(req,res)=>{
+    const id=parseInt(req.params.id);
+    const selectedRecipe=recipe.filter(n => parseInt(n.id) === id)
+    res.send(selectedRecipe)
+})
 app.get('/',(req,res)=>{
     res.send('Chef-Recipe-Hunter In On.....')
 })
